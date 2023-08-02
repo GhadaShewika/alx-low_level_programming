814be963e21b923277bcf828b67d23cd11f0e6f3
Ghada Shewika <ghadashewikagmail.com>
2023-08-02 04:35:55 -0700

    a

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,12 +8,12 @@ testi#include "main.h"
 
 int _strlen_recursion(char *s)
 {
-	int n = 0;
+int ntr = 0;
 
-	if (*s > '\0')
-	{
-		n += _strlen_recursion(s + 1) + 1;
-	}
+if (*s > '\0')
+{
+	ntr += _strlen_recursion(s + 1) + 1;
+}
 
-	return (n);
+return (ntr);
 }
